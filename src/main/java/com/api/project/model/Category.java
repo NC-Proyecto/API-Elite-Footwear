96fddf2707e866b7deeeb3b4a8636f239334b9cd
FedericoG2 <fg0170700@gmail.com>
2024-02-24 15:08:17 -0300

    Datos modelos

--- src/main/java/com/api/project/model/Category.java
+++ src/main/java/com/api/project/model/Category.java
@@ -1,4 +1,15 @@
 package com.api.project.model;
 
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Category {
+    private Long id;
+    private String name;
 }
